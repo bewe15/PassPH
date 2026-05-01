e52b09d55e809e4d07a0eaece652f731693b6343
Viroel Marcelo <viroel@moneyme.com.au>
2026-05-02 00:43:40 +1000

    Fix ESLint ternary expression error in history page

--- app/history/[id]/page.tsx
+++ app/history/[id]/page.tsx
@@ -120,7 +120,7 @@ function WritingResult({ result }: { result: any }) {
   function toggleReveal(id: number) {
     setRevealedTasks((prev) => {
       const next = new Set(prev);
-      next.has(id) ? next.delete(id) : next.add(id);
+      if (next.has(id)) { next.delete(id); } else { next.add(id); }
       return next;
     });
   }
